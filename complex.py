d69deb9081f4f16a79fbb3937532fc143a5349ab
Alejandro <felix090915@gmail.com>
2020-03-24 16:36:24 -0600

    Started Init method

---
+++ complex.py
@@ -0,0 +1,9 @@
+class Complex:
+    def __init__(self, realPart, imaginaryPart):
+        # ToDo: Documentation here
+        self.realPart = realPart
+        self.imaginaryPart = imaginaryPart
+
+    # ToDo: __add__, __sub__, __mul__, __div__
+
+    
